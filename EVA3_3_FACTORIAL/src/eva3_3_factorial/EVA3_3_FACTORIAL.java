bd8f03014b8bb3d939a3a8c29dc808840e9adb74
bicho1717 <143564077+bicho1717@users.noreply.github.com>
2023-11-23 17:02:01 -0700

    Add files via upload

---
+++ EVA3_3_FACTORIAL/src/eva3_3_factorial/EVA3_3_FACTORIAL.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva3_3_factorial;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA3_3_FACTORIAL {
+
+   /**
+    * @param args the command line arguments
+    */
+   public static void main(String[] args) {
+      // TODO code application logic here
+      int valor = 5;
+      System.out.println("Factorial de 5 = " + calcularFactorial(valor));
+      System.out.println("Potencia es igual a  = " + calcularPotencia(2,4));
+   }
+   
+   public static int calcularFactorial(int valor){
+      int guardar = 1;
+      for (int i = 1; i <= valor; i++) {
+         guardar *= i; //guardar = guardar * i
+      }
+      return guardar;
+   }
+   
+   public static int calcularPotencia(int base, int potencia){
+      int resu = 1;
+      for (int i = 1; i <= potencia; i++) {
+         resu = (resu * base);
+      }
+         return resu;
+
+   }
+   
+}
